1c5fee2a4057d9e480acf141b5ca0a289a8ae9b2
denisskarednyh <deniska2019@yandex.ru>
2022-06-12 22:13:01 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
+
     public static void main(String[] args) {
-
-
+        boolean result = Basic.first("java_skyprogo", "D_1hWiKjjP_9", "D_1hWiKjjP_9");
+        if (result) {
+            System.out.println("Все введено корректно");
+        } else {
+            System.out.println("Исправьте логин или пароль");
+        }
     }
 }
